8f39ba98b6a25a54712f0cbb62b664229120aa2a
JJeegs <jhonnelmejor110@gmail.com>
2022-03-10 23:10:26 -0500

    bug fixed de la funcion changeTheme

--- javaScript/changeTheme.js
+++ javaScript/changeTheme.js
@@ -1,31 +1,38 @@
+// traemos el body con el ID "head" para poder escribir en el, y la direccion de nuestro servidor restando extras como el nombre del archivo
+const themeHTML = document.getElementById("theme");
 const head = document.getElementById("head");
 const href = location.href.replace("index.html", "").replace("#", "");
 
+// Asignamos variables a las direcciones desde la raiz del proyecto de los css de cada tema 
 const light = href +"style/themes/theme-light.css";
 const dark = href + "style/themes/theme-dark.css";
 
+// asignamos un espacio en memoria del LocalStorage
 let Theme = localStorage.getItem("Theme");
 
+// asignamos una funcion para tener un tema apenas iniciemos la pagina
 function defaultTheme () {
     if (Theme) {
-        head.innerHTML += "<link id=\"theme\" rel=\"stylesheet\" href=" + light + ">";
+        themeHTML.href = light;
     } else if (Theme == false){
-        head.innerHTML += "<link id=\"theme\" rel=\"stylesheet\" href=" + dark + ">";
+        themeHTML.href = dark;
     }
 }
 
+// asignamos un espacio en memoria del LocalStorage
 Object.defineProperty(localStorage, "Theme", {
     value: true,
     writable: true,
 })
 
+// creamos una funcion para cambiar el tema cada vez q se presiona el boton 
 function changeTheme () {
     if (Theme){
-        localStorage.setItem('Theme', false);
+        Theme = false;
         themeHTML.href = dark;
     } 
-    else{
-        localStorage.setItem('Theme', true);
+    else if (Theme == false){
+        Theme = true;
         themeHTML.href = light;
     }
 }
@@ -41,4 +48,3 @@ function changeTheme () {
 } */
 
 defaultTheme();
-const themeHTML = document.getElementById("theme");
